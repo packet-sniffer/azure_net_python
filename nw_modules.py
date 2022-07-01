a607d9a0f03fb8a45d966defae5f6170fb30ddc7
Rohit Sijoria <rohit.sijoria@pearson.com>
2022-07-01 14:14:12 -0400

    added requirements.txt

--- nw_modules.py
+++ nw_modules.py
@@ -1,11 +1,12 @@
 #/usr/bin/python3.8
 #modules for running different az network commands
 
+import subprocess
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.subscription import SubscriptionClient
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.resource import ResourceManagementClient
-
+from pprint import pprint
 
 # Acquire a credential object
 credential = DefaultAzureCredential()
@@ -17,6 +18,7 @@ def get_subs():
     sub_list = []
     for group in sub_group:
         sub_list.append(group.subscription_id)
+    print(sub_list)
     return sub_list
 
 
@@ -29,17 +31,36 @@ def get_lb(sub_list2):
             res_gr_list.append(item.name)
     print(res_gr_list)
 
+'''
 def get_lb_sub(sub_list2):
     lb_list = []
     for sub in sub_list2:
         net_client = NetworkManagementClient(credential, sub)
         lb = net_client.load_balancers
-        print(lb.type)
+        print(lb)
+'''
+
+
+
+def load_balancer_list(sub_list):
+    lb_list = []
+    for sub in sub_list:
+        try:
+            lb = subprocess.check_output(f'az network lb list --subscription {sub} --output table', stderr=subprocess.STDOUT, shell='True')
+            lb = lb.decode('utf-8')
+            lb = lb.split()[16]
+            lb_list.append(lb)
+        except:
+            pass
+    pprint(lb_list)
+
 
 def main():
     if __name__ == "__main__":
         test_list = get_subs()
+        #test_list = ['39f9beb3-6ab9-4b94-9011-9a185d46e35f']
         #get_lb(test_list)
-        get_lb_sub(test_list)
+        #get_lb_sub(test_list)
+        load_balancer_list(test_list)
 
 main()
